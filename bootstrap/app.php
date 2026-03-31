fcba2d828fb4572ab7d2fa0885335182a268c2a3
HoangLoc2704 <kiyoshilv123@gmail.com>
2026-03-31 16:03:28 +0700

    Https able

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'auth.check' => \App\Http\Middleware\AuthCheck::class,
             'role.check' => \App\Http\Middleware\RoleCheck::class,
